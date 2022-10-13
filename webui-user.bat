c745b0ba2288805b16c192cd90150de7fcc718ee
Nick Clark <nickrbclark@gmail.com>
2022-10-13 09:36:58 -0400

    user auth testing

--- webui-user.bat
+++ webui-user.bat
@@ -3,6 +3,6 @@
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=--listen --gradio-auth="admin:admin"
+set COMMANDLINE_ARGS=--listen --hide-ui-dir-config --gradio-auth="admin:admin,guest:guest,test:test"
 
 call webui.bat
